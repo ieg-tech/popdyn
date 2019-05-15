d8679345a43f1292d4c7faf6a2c60805393bf7cd
cairnsdevin <dcairns@alces.ca>
2019-05-15 13:02:11 -0700

    Test dask changes

--- popdyn/util.py
+++ popdyn/util.py
@@ -62,43 +62,45 @@ def store(sources, targets):
     # return
     # -------------
 
+    da.store(sources, targets, compute=True)
+
     # Optimize all sources together
-    sources_dsk = sharedict.merge(*[e.__dask_graph__() for e in sources])
-    sources_dsk = da.core.Array.__dask_optimize__(
-        sources_dsk,
-        list(core.flatten([e.__dask_keys__() for e in sources]))
-    )
-
-    # Optimize all targets together
-    targets2 = targets
-    targets_keys = []
-    targets_dsk = []
-
-    targets_dsk = sharedict.merge(*targets_dsk)
-    targets_dsk = Delayed.__dask_optimize__(targets_dsk, targets_keys)
-
-    store_keys = []
-    store_dsk = []
-    for tgt, src, reg in zip(targets2, sources, [None] * len(sources)):
-        src = da.core.Array(sources_dsk, src.name, src.chunks, src.dtype)
-
-        each_store_dsk = da.core.insert_to_ooc(
-            src, tgt, lock=True, region=reg,
-            return_stored=False, load_stored=False
-        )
-
-        # Ensure the store keys are unique with a uuid, as duplicate graphs will result in missed stores
-        each_store_dsk = {(key[0] + '-' + str(uuid.uuid4()),) + key[1:]: val
-                          for key, val in each_store_dsk.items()}
-
-        store_keys.extend(each_store_dsk.keys())
-        store_dsk.append(each_store_dsk)
-
-    store_dsk = sharedict.merge(*store_dsk)
-    store_dsk = sharedict.merge(store_dsk, targets_dsk, sources_dsk)
-
-    name = 'store-' + tokenize(*store_keys)
-    dsk = sharedict.merge({name: store_keys}, store_dsk)
-    result = Delayed(name, dsk)
-
-    result.compute()
+    # sources_dsk = sharedict.merge(*[e.__dask_graph__() for e in sources])
+    # sources_dsk = da.core.Array.__dask_optimize__(
+    #     sources_dsk,
+    #     list(core.flatten([e.__dask_keys__() for e in sources]))
+    # )
+    #
+    # # Optimize all targets together
+    # targets2 = targets
+    # targets_keys = []
+    # targets_dsk = []
+    #
+    # targets_dsk = sharedict.merge(*targets_dsk)
+    # targets_dsk = Delayed.__dask_optimize__(targets_dsk, targets_keys)
+    #
+    # store_keys = []
+    # store_dsk = []
+    # for tgt, src, reg in zip(targets2, sources, [None] * len(sources)):
+    #     src = da.core.Array(sources_dsk, src.name, src.chunks, src.dtype)
+    #
+    #     each_store_dsk = da.core.insert_to_ooc(
+    #         src, tgt, lock=True, region=reg,
+    #         return_stored=False, load_stored=False
+    #     )
+    #
+    #     # Ensure the store keys are unique with a uuid, as duplicate graphs will result in missed stores
+    #     each_store_dsk = {(key[0] + '-' + str(uuid.uuid4()),) + key[1:]: val
+    #                       for key, val in each_store_dsk.items()}
+    #
+    #     store_keys.extend(each_store_dsk.keys())
+    #     store_dsk.append(each_store_dsk)
+    #
+    # store_dsk = sharedict.merge(*store_dsk)
+    # store_dsk = sharedict.merge(store_dsk, targets_dsk, sources_dsk)
+    #
+    # name = 'store-' + tokenize(*store_keys)
+    # dsk = sharedict.merge({name: store_keys}, store_dsk)
+    # result = Delayed(name, dsk)
+    #
+    # result.compute()
